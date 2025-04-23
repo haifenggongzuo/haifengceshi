7bfde67ba373a0d6cf710e2afd496c1ba5239fe4
haifenggongzuo <haifeng7741@gmail.com>
2025-04-23 11:33:59 +0700

    Delete deployable_project directory

--- deployable_project/app/api/v1/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-api_v1_bp = Blueprint('api_v1', __name__)
-
-from . import auth, user, data 
--- deployable_project/app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .user import AdminUser, Role, Permission
-from .data import DataRecord, DataSource
-
-__all__ = ['AdminUser', 'Role', 'Permission', 'DataRecord', 'DataSource'] 
